5b4874165972e6affab77a3daa6fc4bf12139711
xoselolo <1a09lopxose@gmail.com>
2019-04-15 23:17:48 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/sbc/xoselolo/lshelpy_chatbot/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sbc.xoselolo.lshelpy_chatbot
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
